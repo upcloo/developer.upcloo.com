f962b19b01a2fedaaff2f4d3935a600df50db86b
Walter Dal Mut <walter.dalmut@gmail.com>
2012-05-19 19:25:16 +0200

    Added base and commits

---
+++ plugins/Provider/Commit.php
@@ -0,0 +1,21 @@
+<?php 
+namespace PhroznPlugin\Provider;
+
+class Commit
+extends \Phrozn\Provider\Base
+implements \Phrozn\Provider
+{
+    public function get()
+    {
+        $config = $this->getConfig();
+    
+        $list =  @json_decode(file_get_contents($config["api"]));
+        if (is_array($list)) {
+            $chunk = array_chunk($list, 5);
+            return $chunk[0];
+        } else {
+            return array();
+        }
+        
+    }
+}
---
+++ plugins/Provider/Sdk.php
@@ -0,0 +1,24 @@
+<?php 
+namespace PhroznPlugin\Provider;
+
+class Sdk
+extends \Phrozn\Provider\Base
+implements \Phrozn\Provider
+{
+    private $_map = array(
+        'php' => array(
+            'issues' => array()
+        )        
+    );
+    
+    public function get()
+    {
+        $config = $this->getConfig();
+    
+        $sdk = $this->_map[$config["platform"]];
+        
+        $sdk["issues"] = @json_decode(file_get_contents($config["issues"]));
+    
+        return $sdk;
+    }
+}
