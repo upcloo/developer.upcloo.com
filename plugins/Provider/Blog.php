fe387561d4af5f9fed59be220cfc6ff8a4879673
Walter Dal Mut <walter.dalmut@gmail.com>
2012-05-19 14:34:10 +0200

    Home and blog

--- plugins/Provider/Blog.php
+++ plugins/Provider/Blog.php
@@ -14,13 +14,16 @@ class Blog
         $xml = simplexml_load_file("http://walterdalmut.com/feed");
         $paths = $xml->xpath("/rss/channel/item");
         
-        foreach ($paths as $path) {
+        foreach ($paths as $i =>$path) {
+            if ($i > 4) break;
             $date = strtotime((string)$path->pubDate);
             
             $data = array(
                 'title' => (string)$path->title,
                 'link' => (string)$path->link,
                 'pubDate' => date("Y-m-d H:i:s", $date),
+                'month' => date('M', $date),
+                'day' => date('d', $date),
                 'summary' => (string)$path->description 
             );
             $results[] = $data;
